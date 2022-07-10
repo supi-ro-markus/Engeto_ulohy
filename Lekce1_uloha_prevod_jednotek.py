8726e528657239680e486d8a9a975131230fd458
supi-ro-markus <mareksupak@icloud.com>
2022-07-10 16:24:01 +0200

    add parameters, calculation and final massage

---
+++ Lekce1_uloha_prevod_jednotek.py
@@ -0,0 +1,22 @@
+# Prevodni pomery
+kg_lb = 2.20
+km_mile = 0.62
+l_gal = 0.26
+
+# Pocet jednotek, ktery ma byt preveden.
+kg_pocet=80
+km_pocet=54
+l_pocet=5
+
+# Vypocty pro prevod
+kg_vysledek=kg_pocet*kg_lb
+km_vysledek=km_pocet*km_mile
+l_vysledek=l_pocet*l_gal
+
+# Vysledne odpovedi
+print("80 kg je:", kg_vysledek,"lb. "
+"54 km je:", km_vysledek,"mil. "
+"5 litrov je:",l_vysledek,"gal."
+    )
+
+# kg_vysledek , " mil. "5 litrov je:"+l_vysledek+" gal."
